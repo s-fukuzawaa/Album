9beab4eb669303fe9d96ccaddcde45747613aa49
Airei Fukuzawa <eyeray@fb.com>
2022-07-06 14:18:12 -0700

    Initial Commit

---
+++ Album/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Album
+//
+//  Created by Airei Fukuzawa on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Album/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Album
+//
+//  Created by Airei Fukuzawa on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
