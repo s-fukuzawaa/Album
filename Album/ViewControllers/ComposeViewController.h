d5d8dc673f60aa8e9c0ec9de8520a84e4e098ff6
Airei Fukuzawa <eyeray@fb.com>
2022-07-07 10:37:48 -0700

    [Skeleton] Added all necessary screens and corresponding view controllers.

---
+++ Album/ViewControllers/ComposeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ComposeViewController.h
+//  Album
+//
+//  Created by Airei Fukuzawa on 7/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
