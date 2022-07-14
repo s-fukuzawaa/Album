e12b32f7a8f2fddd7d7ee0fc20fb859d48d91f8c
Airei Fukuzawa <eyeray@fb.com>
2022-07-14 13:13:42 -0700

    [Friend/Git] Implemented backend to add friendships. Issue: Button UI not changing. Added xcdata as git ignore.

---
+++ Album/Models/Friendship.h
@@ -0,0 +1,16 @@
+//
+//  Friendship.h
+//  Album
+//
+//  Created by Airei Fukuzawa on 7/14/22.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Friendship : PFObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Album/Models/Friendship.m
@@ -0,0 +1,12 @@
+//
+//  Friendship.m
+//  Album
+//
+//  Created by Airei Fukuzawa on 7/14/22.
+//
+
+#import "Friendship.h"
+
+@implementation Friendship
+
+@end
