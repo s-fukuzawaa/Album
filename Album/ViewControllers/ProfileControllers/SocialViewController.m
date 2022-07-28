2c64c5d244636203c101df4b70f36f8b6d72d06b
Airei Fukuzawa <eyeray@fb.com>
2022-07-27 17:03:56 -0700

    [Close Friend] Added view controller for additional close friend feature.

---
+++ Album/ViewControllers/ProfileControllers/SocialViewController.m
@@ -0,0 +1,34 @@
+//
+//  SocialViewController.m
+//  Album
+//
+//  Created by Airei Fukuzawa on 7/27/22.
+//
+
+#import "SocialViewController.h"
+
+@interface SocialViewController ()
+
+@end
+
+@implementation SocialViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)backButton:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
